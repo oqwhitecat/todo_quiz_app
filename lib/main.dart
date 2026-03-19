422cf04015a12a992bf3fd1ebafd9d8eea461775
oqwhirecat <oqwhitecatl@gmail.com>
2026-03-20 05:52:04 +0700

    Fixed SDK and completed Quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,52 @@
 import 'package:flutter/material.dart';
+import 'database_helper.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(home: TodoApp(), debugShowCheckedModeBanner: false));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TodoApp extends StatefulWidget { @override _TodoAppState createState() => _TodoAppState(); }
+class _TodoAppState extends State<TodoApp> {
+  List<Todo> _todos = [];
+  void _refresh() async { final d = await DatabaseHelper.instance.getAll(); setState(() => _todos = d); }
+  @override void initState() { super.initState(); _refresh(); }
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _showForm({Todo? todo}) {
+    var con = TextEditingController(text: todo?.title ?? '');
+    showDialog(context: context, builder: (c) => AlertDialog(
+      title: Text(todo == null ? 'เพิ่มงาน' : 'แก้ไขงาน'),
+      content: TextField(controller: con),
+      actions: [
+        TextButton(onPressed: () => Navigator.pop(c), child: Text('ยกเลิก')),
+        ElevatedButton(onPressed: () async {
+          if (todo == null) await DatabaseHelper.instance.insert(Todo(title: con.text));
+          else { todo.title = con.text; await DatabaseHelper.instance.update(todo); }
+          Navigator.pop(c); _refresh();
+        }, child: Text('บันทึก'))
+      ],
+    ));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  @override Widget build(BuildContext context) {
+    final p = _todos.where((t) => t.isDone == 0).toList();
+    final f = _todos.where((t) => t.isDone == 1).toList();
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      appBar: AppBar(title: Text('To-Do Quiz')),
+      body: ListView(children: [
+        ListTile(title: Text("งานที่ค้าง (${p.length})", style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold))),
+        ...p.map((t) => _tile(t)),
+        Divider(),
+        ListTile(title: Text("เสร็จแล้ว (${f.length})", style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold))),
+        ...f.map((t) => _tile(t)),
+      ]),
+      floatingActionButton: FloatingActionButton(onPressed: () => _showForm(), child: Icon(Icons.add)),
     );
   }
+
+  Widget _tile(Todo t) => Card(child: ListTile(
+    leading: Checkbox(value: t.isDone == 1, onChanged: (v) async { t.isDone = v! ? 1 : 0; await DatabaseHelper.instance.update(t); _refresh(); }),
+    title: Text(t.title, style: TextStyle(decoration: t.isDone == 1 ? TextDecoration.lineThrough : null)),
+    trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+      IconButton(icon: Icon(Icons.edit, color: Colors.blue), onPressed: () => _showForm(todo: t)),
+      IconButton(icon: Icon(Icons.delete, color: Colors.red), onPressed: () async { await DatabaseHelper.instance.delete(t.id!); _refresh(); }),
+    ]),
+  ));
 }
